55ec97216b8ca4652d24cd7c427a24ffd0e402ba
jmay1 <jmay1@terpmail.umd.edu>
2020-04-06 22:26:13 -0400

    Added Darkmode

--- public/js/script.js
+++ public/js/script.js
@@ -3,7 +3,40 @@ function openNav() {
     document.getElementById("myOverlay").style.display = "block";
   }
   
-  function closeNav() {
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
+function closeNav() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
+
+window.addEventListener('load', () => {
+  const checkbox = document.querySelector("#dark");
+  if (sessionStorage.getItem("mode") == "dark") {
+    darkmode(); 
+  } else {
+    nodark(); 
   }
+  checkbox.addEventListener("change", function() {
+    if (checkbox.checked) {
+      darkmode(); 
+    } else {
+      nodark(); 
+    }
+  });
+  function darkmode() {
+    document.body.classList.add("dark-mode"); 
+    checkbox.checked = true; 
+    sessionStorage.setItem("mode", "dark");
+  }
+  function nodark() {
+    document.body.classList.remove("dark-mode"); 
+    checkbox.checked = false; 
+    sessionStorage.setItem("mode", "light"); 
+  }
+});
+
+
+
+
+
+
+
